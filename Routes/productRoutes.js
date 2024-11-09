641ade453df245bc6a135cabd940607edc9f6568
Hakan MertÖzcan <hakanmertozcan@gmail.com>
2024-11-09 16:03:43 +0300

    Some fixes to routing and stuff, new api needs testing will handle it today

--- Routes/productRoutes.js
+++ Routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const productController = require('../Controllers/productController');
+const productController = require('../Controller/productController');
 
 router.get('/:id', productController.getProductById);
 
