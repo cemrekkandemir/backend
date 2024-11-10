cb18683bee2360db7c6f86624ba1095f8cbff6fb
Hakan MertÖzcan <hakanmertozcan@gmail.com>
2024-11-10 14:10:22 +0300

    API to create a product is done

--- Controller/productController.js
+++ Controller/productController.js
@@ -1,7 +1,7 @@
 const Product = require('../Models/Product');
 
 
-// Get a product's details by ID
+// Get a product's details by ID (GET)
 exports.getProductById = async (req, res) => {
     try {
         const product = await Product.findById(req.params.id);
@@ -11,3 +11,15 @@ exports.getProductById = async (req, res) => {
             res.status(500).json({ message: error.message });
         }
     }
+
+// Create a new product (POST)
+exports.createProduct = async (req, res) => {
+    const { name, description, price, category, brand, stock, imageURL } = req.body;
+    const newProduct = new Product({ name, description, price, category, brand, stock, imageURL });
+    try {
+        const savedProduct = await newProduct.save();
+        res.status(201).json(savedProduct);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
