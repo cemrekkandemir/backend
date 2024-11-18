2d6ae16f4b9a77290d91d510bdebcda2fad984de
sude kumbasar <sudekumbasar@gmail.com>
2024-11-18 16:56:15 +0300

    Updated cart model

--- Models/Cart.js
+++ Models/Cart.js
@@ -1,8 +1,10 @@
+// models/cart.js
+
 const mongoose = require('mongoose');
 
 const cartSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  sessionId: { type: String },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null },
+  guestId: { type: String, default: null }, // Misafir kullanıcılar için
   items: [
     {
       productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
@@ -12,3 +14,5 @@ const cartSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 module.exports = mongoose.model('Cart', cartSchema);
+
+
