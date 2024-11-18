472303b89b3a6500c57127e8bd03168fc1ffd890
sude kumbasar <sudekumbasar@gmail.com>
2024-11-18 17:12:57 +0300

    Updated routes

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -1,11 +1,14 @@
 // routes/authRoutes.js
 
-const express = require('express');
-const { signup, login } = require('../Controller/authController');
-const router = express.Router();
 
-router.post('/signup', signup);
-router.post('/login', login);
+const express = require('express');
+const router = express.Router();
+const authController = require('../Controller/authController');
+const mergeCarts = require('../Middleware/cartMerger');
+
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+router.post('/logout', authController.logout); 
 
 module.exports = router;
 
--- Routes/cartRoutes.js
+++ Routes/cartRoutes.js
@@ -1,15 +1,19 @@
 // cartRoutes.js
+
 const express = require('express');
 const router = express.Router();
 const cartController = require('../Controller/cartController');
 const optionalAuth = require('../Middleware/optionalAuth');
 
 
-router.post('/add', optionalAuth, cartController.addItem);
-router.put('/update', optionalAuth, cartController.updateItem);
-router.delete('/remove', optionalAuth, cartController.removeItem);
+router.use(optionalAuth);
 
 
-router.get('/view', optionalAuth, cartController.viewCart);
+router.post('/add', cartController.addItem);
+router.put('/update', cartController.updateItem);
+router.delete('/remove', cartController.removeItem);
+router.get('/view', cartController.viewCart);
 
 module.exports = router;
+
+
