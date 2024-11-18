7f6a7cd1efe2e5b3cc14abc2f7c9ac0c12da9a5d
sude kumbasar <sudekumbasar@gmail.com>
2024-11-18 17:11:26 +0300

    Updated cart merger

--- Middleware/cartMerger.js
+++ Middleware/cartMerger.js
@@ -1,21 +1,20 @@
 const mergeCarts = async (req, res, next) => {
     try {
-      const userId = req.user?._id; // Giriş yapmış kullanıcı
-      const guestId = req.sessionID; // Misafir kullanıcı ID'si
+      const userId = req.user?._id; 
+      const guestId = req.sessionID; 
   
       if (userId && guestId) {
         console.log("Merging carts for userId:", userId, "and guestId:", guestId);
   
-        // Misafir sepetini al
         const guestCart = await Cart.findOne({ guestId });
         if (guestCart) {
-          // Kullanıcının mevcut sepetini al veya oluştur
+          
           let userCart = await Cart.findOne({ userId });
           if (!userCart) {
             userCart = new Cart({ userId, items: [] });
           }
   
-          // Sepetleri birleştir
+          
           guestCart.items.forEach(guestItem => {
             const existingItem = userCart.items.find(item =>
               item.productId.equals(guestItem.productId)
@@ -26,8 +25,7 @@ const mergeCarts = async (req, res, next) => {
               userCart.items.push(guestItem);
             }
           });
-  
-          // Misafir sepetini sil
+
           await guestCart.deleteOne();
           await userCart.save();
   
