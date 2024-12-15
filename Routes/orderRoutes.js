b858bf7434cea76b77c0c0ce05b1ddb8680e1830
Hakan MertÖzcan <hakanmertozcan@gmail.com>
2024-12-15 20:21:28 +0300

    Changes in order status update api

--- Routes/orderRoutes.js
+++ Routes/orderRoutes.js
@@ -5,7 +5,7 @@ const orderController = require('../Controller/orderController');
 const auth = require('../Middleware/auth');
 
 router.post('/place', auth, orderController.placeOrder);
-router.put('/:orderId/status', auth, orderController.updateOrderStatus);
+router.put('/:orderId/status', orderController.updateOrderStatus);
 router.get('/status', orderController.getLatestOrderStatus);
 router.get('/all', orderController.getAllOrders);
 module.exports = router;
